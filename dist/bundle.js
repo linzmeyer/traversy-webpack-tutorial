b901c7384e8a4e0e1ece72e80edab38186e67fa2
context-coding <context.coding@gmail.com>
2022-03-20 21:21:54 -0500

    update

---
+++ dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{var e={175:e=>{e.exports=[{name:"John"},{name:"Wendy"},{name:"Kyle"}]}},r={};function o(n){var t=r[n];if(void 0!==t)return t.exports;var a=r[n]={exports:{}};return e[n](a,a.exports,o),a.exports}(()=>{let e=o(175);console.log(e)})()})();
---
+++ src/js/people.js
@@ -0,0 +1,11 @@
+// function getHello() {
+//   return 'Hello There'
+// }
+
+let people = [
+  {name: 'John'},
+  {name: 'Wendy'},
+  {name: 'Kyle'},
+]
+
+module.exports = people;
